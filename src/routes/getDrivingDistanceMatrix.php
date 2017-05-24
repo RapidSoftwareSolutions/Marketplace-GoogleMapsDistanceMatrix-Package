8e08a13d848fe0a22110a1272228e78031f5f613
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-05-24 17:17:41 +0300

    datatypes refactored

--- src/routes/getDrivingDistanceMatrix.php
+++ src/routes/getDrivingDistanceMatrix.php
@@ -1,11 +1,11 @@
 <?php
-$app->post('/api/GoogleMapsDistanceMatrix/getDrivingDistanceMatrix', function($request, $response, $args){
+$app->post('/api/GoogleMapsDistanceMatrix/getDrivingDistanceMatrix', function ($request, $response, $args) {
     $settings = $this->settings;
 
     //checking properly formed json
     $checkRequest = $this->validation;
-    $validateRes = $checkRequest->validate($request, ['apiKey','origins', 'destinations']);
-    if(!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback']=='error') {
+    $validateRes = $checkRequest->validate($request, ['apiKey', 'origins', 'destinations']);
+    if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
         return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
     } else {
         $post_data = $validateRes;
@@ -16,22 +16,24 @@ $app->post('/api/GoogleMapsDistanceMatrix/getDrivingDistanceMatrix', function($r
     $query['mode'] = 'driving';
     $query['origins'] = $post_data['args']['origins'];
     $query['destinations'] = $post_data['args']['destinations'];
-    if(!empty($post_data['args']['language'])) {
+    if (!empty($post_data['args']['language']) && strlen($post_data['args']['language']) > 0) {
         $query['language'] = $post_data['args']['language'];
     };
-    if(!empty($post_data['args']['units'])) {
+    if (!empty($post_data['args']['units']) && strlen($post_data['args']['units']) > 0) {
         $query['units'] = $post_data['args']['units'];
     };
-    if(!empty($post_data['args']['departureTime'])) {
-        $query['departureTime'] = $post_data['args']['departureTime'];
+    if (!empty($post_data['args']['departureTime']) && strlen($post_data['args']['departureTime']) > 0) {
+        if (is_numeric($post_data['args']['departureTime']) || $post_data['args']['departureTime'] == 'now') {
+            $query['departureTime'] = $post_data['args']['departureTime'];
+        } else {
+            $date = new DateTime($post_data['args']['departureTime']);
+            $query['departureTime'] = $date->format('u');
+        }
     };
-    if(!empty($post_data['args']['departureTime']) && !empty($post_data['args']['trafficModel'])) {
+    if (!empty($post_data['args']['departureTime']) && !empty($post_data['args']['trafficModel']) && strlen($post_data['args']['trafficModel']) > 0) {
         $query['trafficModel'] = $post_data['args']['trafficModel'];
     };
-    if(!empty($post_data['args']['avoid'])) {
-        $query['avoid'] = $post_data['args']['avoid'];
-    };
-    if(!empty($post_data['args']['avoid'])) {
+    if (!empty($post_data['args']['avoid']) && strlen($post_data['args']['avoid']) > 0) {
         $query['avoid'] = $post_data['args']['avoid'];
     };
 
@@ -41,7 +43,7 @@ $app->post('/api/GoogleMapsDistanceMatrix/getDrivingDistanceMatrix', function($r
     $client = $this->httpClient;
     try {
 
-        $resp = $client->get( $query_str,
+        $resp = $client->get($query_str,
             [
                 'query' => $query,
                 'verify' => false
@@ -52,7 +54,7 @@ $app->post('/api/GoogleMapsDistanceMatrix/getDrivingDistanceMatrix', function($r
         $all_data[] = $rawBody;
 
 
-        if(!empty(json_decode($responseBody)->rows) && json_decode($responseBody)->status == 'OK') {
+        if (!empty(json_decode($responseBody)->rows) && json_decode($responseBody)->status == 'OK') {
             $result['callback'] = 'success';
             $result['contextWrites']['to'] = is_array($all_data) ? $all_data : json_decode($all_data);
         } else {
